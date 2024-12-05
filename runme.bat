f99b80081f61fda2c1a91f77f67af80cc62be165
Brian Jayson P. Umandap <brian.umandap@dotrgov.ph>
2024-12-06 07:43:30 +0800

    updated bat to ensure py file is hidden

--- runme.bat
+++ runme.bat
@@ -2,6 +2,9 @@
 :: Change directory to the specific folder
 cd "%~dp0"
 
+:: Hide the Python file to make it invisible
+attrib +h runthis.py
+
 :: Run the Python script using Python
 py runthis.py
 
